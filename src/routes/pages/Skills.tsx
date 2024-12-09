aa2c297d0a70f30b7c24ba53aaa5f72e6a185b51
Adam <adamj1215@gmail.com>
2024-12-09 01:26:00 +0100

    added cool svg

--- src/routes/pages/Skills.tsx
+++ src/routes/pages/Skills.tsx
@@ -1,7 +1,17 @@
+import { Container } from "@chakra-ui/react";
+import { Page } from "@components/Page";
+import { CustomSection } from "@components/section/CustomSection";
+import { SisyphusIcon } from "@icons/SisyphusIcon";
 import { ReactElement } from "react";
 
 const Skills = (): ReactElement => {
-  return <h1>SKILLS</h1>;
+  return (
+    <Page animationDuration={0}>
+      <Container maxW={{ base: "60ch", md: "80ch" }}>
+        <CustomSection headerTranslationKey="skills" children={<SisyphusIcon />} />
+      </Container>
+    </Page>
+  );
 };
 
 export default Skills;
